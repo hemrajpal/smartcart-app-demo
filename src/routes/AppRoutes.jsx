580fff8fea60f7a8cae04bcb634f4fb58fbb0a3a
hemrajpal <68118396+hemrajpal@users.noreply.github.com>
2026-09-02 11:52:07 +0530

    Update AppRoutes.jsx

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -24,6 +24,7 @@ function AppRoutes() {
     <BrowserRouter>
       <Routes>
         {/* Public Routes */}
+        <Route path="/" element={<Navigate to="/home" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
 
@@ -35,7 +36,7 @@ function AppRoutes() {
             </ProtectedRoute>
           }
         >
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
 
           <Route path="/cart" element={<Cart />} />
 
