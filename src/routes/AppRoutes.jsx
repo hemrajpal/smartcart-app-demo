c4545637b0f29bcfe1dd3029051fe041c0c42a34
hemrajpal <68118396+hemrajpal@users.noreply.github.com>
2026-09-02 11:53:54 +0530

    Update AppRoutes.jsx

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
